dfa171650417402103daaf8b51aac5aa8ac282a5
Aleksandr Shalugin <shalugin.alexandr@gmail.com>
2022-08-26 18:09:11 +0300

    Fix default port

--- QueryBuilder.Tests/MySqlInitialization.cs
+++ QueryBuilder.Tests/MySqlInitialization.cs
@@ -13,7 +13,7 @@ public static class MySqlInitialization
         var database = System.Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "master";
         var user = System.Environment.GetEnvironmentVariable("MYSQL_USER") ?? "root";
         var password = System.Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "1";
-        var port = System.Environment.GetEnvironmentVariable("MYSQL_Port") ?? "3306";
+        var port = System.Environment.GetEnvironmentVariable("MYSQL_Port") ?? "13306";
 
         var testContainer = new TestcontainersBuilder<MySqlTestcontainer>()
             .WithDatabase(new MySqlTestcontainerConfiguration("mysql/mysql-server:latest")
